4e49e711cf70a838a668a64ef7dec87a7e5f005d
Jennifer <jennifereneh05@gmail.com>
2024-03-23 14:32:26 +0100

    Jennifer

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ function dropDown() {
   }
 }
 
-menu.addEventListener("click", () => {
+/*menu.addEventListener("click", () => {
   menu.style.display = "none";
+});
+*/
+
+let vals = document.querySelectorAll(".num");
+let interval = 0;
+
+vals.forEach((val) => {
+  let startValue = 5500;
+  let endvalue = parseInt(val.getAttribute("data-val"));
+  let duration = 1000;
+  let counter = setInterval(function () {
+    startValue += 1;
+    val.textContent = startValue;
+    if (startValue == endvalue) {
+      clearInterval(counter);
+    }
+  }, duration);
 });
