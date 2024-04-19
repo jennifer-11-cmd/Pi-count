9b61e14a6cd63e48cef358b9483f25ee7fd34886
Jennifer <jennifereneh05@gmail.com>
2024-04-19 04:33:02 +0100

    https://github.com/jennifer-11-cmd

--- main.js
+++ main.js
@@ -10,23 +10,3 @@ function dropDown() {
     menu.style.display = "none";
   });
 }
-
-let vals = document.querySelectorAll(".num");
-let interval = 0;
-
-vals.forEach((val) => {
-  let startValue = 3500;
-  let endvalue = parseInt(val.getAttribute("data-val"));
-  let duration = 1000;
-  let counter = setInterval(function () {
-    startValue = localStorage.getItem("startValue");
-    startValue = parseInt(startValue); // because localstorage stores everything in strings
-    startValue += 1;
-    val.textContent = startValue;
-    localStorage.setItem("startValue", startValue);
-
-    if (startValue == endvalue) {
-      clearInterval(counter);
-    }
-  }, duration);
-});
